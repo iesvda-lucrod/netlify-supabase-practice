85f95de4017b5e2d1adbd80af0812c792b575979
lucrod <iesvda.lucrod@gmail.com>
2025-01-20 10:32:19 +0100

    Fixed fetch url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('fetchData').addEventListener('click', async () => {
-    const response = await fetch('https://chhfpnafjajancuedvpi.supabase.co', {
+    const response = await fetch('https://chhfpnafjajancuedvpi.supabase.co/rest/v1/example_table', {
         headers: {
             'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoaGZwbmFmamFqYW5jdWVkdnBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNjM5ODYsImV4cCI6MjA1MjkzOTk4Nn0.lo-uZsUYOpkmh3mUTDwb6_DgsUmBZb7-84UEnEjKt3A',
             'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoaGZwbmFmamFqYW5jdWVkdnBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNjM5ODYsImV4cCI6MjA1MjkzOTk4Nn0.lo-uZsUYOpkmh3mUTDwb6_DgsUmBZb7-84UEnEjKt3A'
