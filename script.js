b50e515bf5da32b0de6a335eeb594c03da402039
lucrod <iesvda.lucrod@gmail.com>
2025-01-20 10:28:00 +0100

    Fixed bearer password

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('fetchData').addEventListener('click', async () => {
     const response = await fetch('https://chhfpnafjajancuedvpi.supabase.co', {
         headers: {
             'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoaGZwbmFmamFqYW5jdWVkdnBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNjM5ODYsImV4cCI6MjA1MjkzOTk4Nn0.lo-uZsUYOpkmh3mUTDwb6_DgsUmBZb7-84UEnEjKt3A',
-            'Authorization': 'Bearer your-supabase-api-key'
+            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoaGZwbmFmamFqYW5jdWVkdnBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNjM5ODYsImV4cCI6MjA1MjkzOTk4Nn0.lo-uZsUYOpkmh3mUTDwb6_DgsUmBZb7-84UEnEjKt3A'
         }
     });
 
